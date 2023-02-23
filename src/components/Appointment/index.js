ffc629d86ad7e249f2fb147a1d3f02db87672b04
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-02-22 22:54:08 -0500

    Add Axio request put and delete

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Axios from "axios";
 
 import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Confirm from "./Confirm";
+import Status from "./Status";
+import Error from "./Error";
 
 import useVisualMode from "hooks/useVisualMode";
 
@@ -14,24 +19,65 @@ const Appointment = ({ id, time, interview, interviewers }) => {
   const CREATE = "CREATE";
   const EDIT = "EDIT";
   const CONFIRM = "CONFIRM";
-  const SAVING = "SAVING";
-  const DELETING = "DELETING";
+  const STATUS = "STATUS";
+  const ERROR = "ERROR";
 
   const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
+  const [selectedInterview, setSelectedInterview] = useState(interview || null);
+  const [loadingMsg, setLoadingMsg] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
   const handleSave = (student, interviewer) => {
-    console.log(student, interviewer);
-    transition(SAVING);
+    // console.log(student, interviewer);
+    setLoadingMsg("Saving...");
+    transition(STATUS);
+
+    Axios.put(`/api/appointments/${id}`, {
+      interview: { student, interviewer },
+    })
+      .then((res) => {
+        const dataObj = JSON.parse(res.config.data);
+        const interviewerId = dataObj.interview.interviewer;
+        const interviewerData = interviewers?.find(
+          (data) => data.id === interviewerId
+        );
+        setSelectedInterview({
+          ...dataObj.interview,
+          interviewer: interviewerData,
+        });
+      })
+      .catch((err) => {
+        transition(ERROR);
+        setErrorMsg(err);
+        back();
+      })
+      .finally(() => transition(SHOW));
+  };
+
+  const handleDelete = () => {
+    setLoadingMsg("Deleting...");
+    transition(STATUS);
+    Axios.delete(`/api/appointments/${id}`)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setErrorMsg(err);
+        transition(ERROR);
+      })
+      .finally(() => transition(EMPTY));
   };
 
   return (
     <article className="appointment">
       <Header time={time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-      {mode === SHOW && interview && (
+      {mode === SHOW && selectedInterview && (
         <Show
-          student={interview.student}
-          interviewer={interview.interviewer}
+          student={selectedInterview.student}
+          interviewer={selectedInterview.interviewer}
           onEdit={() => transition(EDIT)}
           onDelete={() => transition(CONFIRM)}
         />
@@ -48,6 +94,15 @@ const Appointment = ({ id, time, interview, interviewers }) => {
           onCancel={back}
         />
       )}
+      {mode === CONFIRM && (
+        <Confirm
+          message="Are you sure you want to delete this appointment?"
+          onConfirm={handleDelete}
+          onCancel={back}
+        />
+      )}
+      {mode === STATUS && <Status message={loadingMsg} />}
+      {mode === ERROR && <Error message={errorMsg} onClose={back} />}
     </article>
   );
 };
